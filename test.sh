7e5a6479f60387f783170dd96d4b8ca7f3a5ada3
NAKAJIMA Yusaku <yupong7@gmail.com>
2020-08-13 12:07:08 +0900

    refactoring.

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
 LANG=C
 
 function error() {
-    echo "$@" >&2
+    echo "Error: $@" >&2
     exit 1
 }
 
@@ -13,54 +13,54 @@ function runtest() {
     cmp tmp.0 tmp.1 || error "$3"
 }
 
-runtest  0  0 "Error: $LINENO"
-runtest 42 42 "Error: $LINENO"
-runtest '1 2 3' '1 2 3' "Error: $LINENO"
+runtest  0  0           "$LINENO"
+runtest 42 42           "$LINENO"
+runtest '1 2 3' '1 2 3' "$LINENO"
 
-runtest   3 '+ 1 2'             "Error: $LINENO"
-runtest   6 '+ + 1 2 3'         "Error: $LINENO"
-runtest   6 '++1 2 3'           "Error: $LINENO"
+runtest   3 '+ 1 2'             "$LINENO"
+runtest   6 '+ + 1 2 3'         "$LINENO"
+runtest   6 '++1 2 3'           "$LINENO"
 
-runtest   1 '- 5 4'             "Error: $LINENO"
-runtest  -9 '- 6 + 7 8'         "Error: $LINENO"
+runtest   1 '- 5 4'             "$LINENO"
+runtest  -9 '- 6 + 7 8'         "$LINENO"
 
-runtest  90 '* 9 10'            "Error: $LINENO"
-runtest 621 '* + 11 12 + 13 14' "Error: $LINENO"
+runtest  90 '* 9 10'            "$LINENO"
+runtest 621 '* + 11 12 + 13 14' "$LINENO"
 
-runtest   3 '/ 15 5'            "Error: $LINENO"
-runtest   2 '/ 16 6'            "Error: $LINENO"
-runtest   0 '/  0 17'           "Error: $LINENO"
+runtest   3 '/ 15 5'            "$LINENO"
+runtest   2 '/ 16 6'            "$LINENO"
+runtest   0 '/  0 17'           "$LINENO"
 
 # Functions
-runtest  32 'F[32]    F()'        "Error: $LINENO"     
-runtest   1 'F[a]     F(1)'       "Error: $LINENO"
-runtest  36 'F[* a 2] F(18)'      "Error: $LINENO"
-runtest 256 'F[* a a] F(F(F(2)))' "Error: $LINENO"
+runtest  32 'F[32]    F()'        "$LINENO"     
+runtest   1 'F[a]     F(1)'       "$LINENO"
+runtest  36 'F[* a 2] F(18)'      "$LINENO"
+runtest 256 'F[* a a] F(F(F(2)))' "$LINENO"
 
 runtest 321 'F[* a  a] G[+ a a] - F(19) G(20)' "$LINENO"
 runtest  42 'F[+ a  a] G[F(a)]       G(21)'    "$LINENO"
 runtest  12 'F[+a 22]G[F(*a 23)] /G(24)F(25)'  "$LINENO"
 
-runtest  53 'F[+ a b] F(26 27)'       "Error: $LINENO"
+runtest  53 'F[+ a b] F(26 27)'                     "$LINENO"
 runtest 751 'F[* a b] G[+ a b] - F(28 29) G(30 31)' "$LINENO"
 
 # Error
 # 
 # divided by 0
-./lang '/ 18 0'     2>/dev/null && error "Error: $LINENO"
-./lang '/ 19 - 7 7' 2>/dev/null && error "Error: $LINENO"
+./lang '/ 18 0'     2>/dev/null && error "$LINENO"
+./lang '/ 19 - 7 7' 2>/dev/null && error "$LINENO"
 
-./lang a 2>/dev/null && error "Error: accept digit only"
+./lang a 2>/dev/null && error "accept digit only"
 
-./lang '+ 1' 2>/dev/null && error "Error: must not null"
+./lang '+ 1' 2>/dev/null && error "must not null"
 
-./lang 2>/dev/null && error "Error: no input program"
+./lang 2>/dev/null && error "no input program"
 
 ./lang 'F[' 2>/dev/null \
-    && error "Error: expect ']' before EOF"
+    && error "expect ']' before EOF"
 
 ./lang 'F[.] F(2]' 2>/dev/null \
-    && error "Error: ')' expected: \"]]\""       
+    && error "')' expected: \"]]\""       
 
 rm tmp.{0,1}
 
